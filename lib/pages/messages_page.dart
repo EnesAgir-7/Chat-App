d6d9646db0bf113d59a6a7f7840bfb40d0998cc2
Enes AGIR <enes.agir.7@gmail.com>
2022-06-20 01:09:28 +0200

    stories list builder

--- lib/pages/messages_page.dart
+++ lib/pages/messages_page.dart
@@ -1,12 +1,92 @@
+import 'package:chat_app/widgets/widgets.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+
+import '../helpers.dart';
+import '../models/models.dart';
+import '../theme.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("messagesPage"),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: _Stories(),
+    );
+  }
+}
+
+//^ ' _ ' meaning private
+class _Stories extends StatelessWidget {
+  const _Stories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      child: SizedBox(
+        height: 135,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 16,bottom: 16,top: 8),
+              child: Text(
+                'Stories',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w900, color: AppColors.textFaded),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    //^ fake is get fake information
+                    final faker = Faker();
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(width: 60, child: _StoryCard(storyData: StoryData(name: faker.person.name(),url:Helpers.randomPictureUrl()))),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _StoryCard extends StatelessWidget {
+  const _StoryCard({
+    Key? key,
+    required this.storyData,
+  }) : super(key: key);
+
+  final StoryData storyData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Avatar.medium(url: storyData.url),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: Text(
+              storyData.name,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 11,
+                letterSpacing: 0.3,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
